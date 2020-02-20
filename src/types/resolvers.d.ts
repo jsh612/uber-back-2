6f78d4baea0e346d0afcee9f6bffca8b38c17b4e
seungho Jeon <urijsh612@gmail.com>
2020-02-20 23:50:43 +0900

    #1.25 Resolver Types (이전 커밋과 같이 보기)

--- src/types/resolvers.d.ts
+++ src/types/resolvers.d.ts
@@ -1,4 +1,5 @@
-// graphql-tools 에 이미 resolver 관련 사항 있음 참고
+// graphql-tools 에 이미 resolver 관련 사항 있음
+// 밑에 작성된것과 graphql-tools에 있는 것중 어떤거를 써야하는지 나중에 다시 보기
 // https://www.apollographql.com/docs/graphql-tools/resolvers/#resolver-map
 export type Resolver = (parent: any, args: any, context: any, info: any) => any;
 
