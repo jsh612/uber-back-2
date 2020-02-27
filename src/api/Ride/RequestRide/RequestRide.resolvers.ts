e4bb3e480ac125bef35ef950d082ddd95097b5ca
seungho Jeon <urijsh612@gmail.com>
2020-02-27 17:11:10 +0900

    #1.72 RequestRide Resolver

---
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -0,0 +1,41 @@
+import {
+  RequestRideMutationArgs,
+  RequestRideResponse
+} from "./../../../types/graph.d";
+import { IResolvers } from "graphql-tools";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import Ride from "../../../entities/Ride";
+
+const resolvers: IResolvers = {
+  Mutation: {
+    RequestRide: privateResolver(
+      async (
+        _,
+        args: RequestRideMutationArgs,
+        { request }
+      ): Promise<RequestRideResponse> => {
+        const user: User = request;
+        try {
+          const ride: Ride = await Ride.create({
+            ...args,
+            passenger: user
+          }).save();
+          return {
+            ok: true,
+            error: null,
+            ride
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            ride: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
