059c1511d2a991477bb47d2b726612a105db3bc0
seungho Jeon <urijsh612@gmail.com>
2020-02-28 17:35:28 +0900

    #1.85 SendChatMessage Resolver

--- README.md
+++ README.md
@@ -37,8 +37,8 @@
 - [x] Subscribe to Ride Status
 - [x] Create a Chat Room
 - [x] Get Chat Room Messages
+- [x] Send a Chat Message
 - [ ] Subscribe to Chat Room Messages
-- [ ] Send a Chat Message
 
 ---
 
---
+++ src/api/Chat/SendChatMessage/SendChatMessage.resolvers.ts
@@ -0,0 +1,60 @@
+import {
+  SendChatMessageMutationArgs,
+  SendChatMessageResponse
+} from "./../../../types/graph.d";
+import { IResolvers } from "graphql-tools";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import Chat from "../../../entities/Chat";
+import Message from "../../../entities/Message";
+
+const resolvers: IResolvers = {
+  Mutation: {
+    SendChatMessage: privateResolver(
+      async (
+        _,
+        args: SendChatMessageMutationArgs,
+        { request }
+      ): Promise<SendChatMessageResponse> => {
+        const user: User = request.user;
+        try {
+          const chat = await Chat.findOne({ id: args.chatId });
+          if (chat) {
+            if (chat.passengerId === user.id || chat.driverId === user.id) {
+              const message = await Message.create({
+                text: args.text,
+                chat,
+                user
+              }).save();
+              return {
+                ok: true,
+                error: null,
+                message
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Unauthorized",
+                message: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Chat not found",
+              message: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            message: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
